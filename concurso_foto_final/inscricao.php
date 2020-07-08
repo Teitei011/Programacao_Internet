85b6c3b28be1ff853d1f1f10a985a6adf4858e8e
Teitei011 <stefantleal14@gmail.com>
2020-07-08 19:39:42 -0300

    fdshfdkj

--- concurso_foto_final/inscricao.php
+++ concurso_foto_final/inscricao.php
@@ -1,29 +1,27 @@
 <?php
-    session_start();
-    echo "<center>";
-    $string = "TEstenkdjsahdkljsa" +  date("Y.m.d");
-    $token = sha1($string);
-    $_SESSION['token'] = $token;
-    
-  
-    $nome = $_GET["nome"];
-    $email = $_GET["email"];
-    $senha = $_GET["senha"];
-    $telefone = $_GET["telefone"];
+  session_start();
+  echo "<center>";
+  $string = "TEstenkdjsahdkljsa" +  date("Y.m.d");
+  $token = sha1($string);
+  $_SESSION['token'] = $token;
 
-    $db = new SQLite3('database.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
-    $senhaCriptografada = sha1($senha);
+  $nome = $_GET["nome"];
+  $email = $_GET["email"];
+  $senha = $_GET["senha"];
 
-   $db->exec("INSERT INTO Artist (name, email, senha) VALUES ('$nome', '$email', '$senhaCriptografada';");
-    $db->close();
+  $db = new SQLite3('database.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+
+  $senhaCriptografada = sha1($senha);
+  $db->exec("INSERT INTO Artista (name, email, senha) VALUES ('$nome', '$email', '$senhaCriptografada';");
+  $db->close();
 ?> 
 
 
 <html>
 <head><title>Inscrição</title></head>
 
-<body background="images/backclass1.gif" bgcolor="#FFFFFF"
+<body background="assets/img/backclass1.gif" bgcolor="#FFFFFF"
   vlink="#FF0000" alink="#FF00FF" leftmargin="25">
     <center><h1> Inscrição efetuada com sucesso!</h1>
 
