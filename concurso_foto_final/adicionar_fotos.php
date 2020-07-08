6bdc002514de4566fa0dab82a6ddbfb634b66629
Teitei011 <stefantleal14@gmail.com>
2020-07-08 20:01:08 -0300

    modifications on databse

--- concurso_foto_final/adicionar_fotos.php
+++ concurso_foto_final/adicionar_fotos.php
@@ -12,8 +12,20 @@
 <html>
 
 <body>
-    <form action="">
+    <form action="" method="GET">
+    <p>Concurso a que pertence: 
+
+    <select name="concurso">
+      <option value="0" selected>Natureza</option>
+      <option value="1">Pessoas</option>
+      <option value="2">Outros</option>
+    </select>
+  </p>
+        Título: <input type="text" name=titulo>
+        Descricao: <input type="text" name=descricao>
         <input type="file" name="foto1">
+        <input type="submit" name="Submit" value="Enviar"> 
+
     </form>
 </body>
 
