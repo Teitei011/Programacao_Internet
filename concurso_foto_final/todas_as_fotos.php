a32e4ea32e97f29976d7e80e1ca8afba2f60f4f4
Teitei011 <stefantleal14@gmail.com>
2020-07-09 12:48:38 -0300

    todas as fotos bootstrap

--- concurso_foto_final/todas_as_fotos.php
+++ concurso_foto_final/todas_as_fotos.php
@@ -1,6 +1,21 @@
-<?php
-   session_start();
+<?php  session_start(); ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Login Page</title>
+    <link href="css/login.css" type="text/css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+
+<?php
+  
    $db = new SQLite3('database.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
    $photos= $db->query("SELECT * FROM Photo;");
