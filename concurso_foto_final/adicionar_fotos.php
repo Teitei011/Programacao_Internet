b1766b636eb594a1e088ce95c3085a3cab7e1bee
Teitei011 <stefantleal14@gmail.com>
2020-07-09 16:55:32 -0300

    adicionar foyto

--- concurso_foto_final/adicionar_fotos.php
+++ concurso_foto_final/adicionar_fotos.php
@@ -4,30 +4,33 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <link href="css/login.css" type="text/css" rel="stylesheet"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <title>Concurso de Fotografia</title>
+  <meta charset="utf-8" />
+  <link href="css/login.css" type="text/css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <title>Concurso de Fotografia</title>
 </head>
 
 <html>
 
 <body>
+  <div class="container">
     <form action="add_2_database.php" method="GET">
-    <p>Concurso a que pertence: 
+      <p>Concurso a que pertence:
 
-    <select name="concurso">
-      <option value="0" selected>Natureza</option>
-      <option value="1">Pessoas</option>
-      <option value="2">Outros</option>
-    </select>
-  </p>
-        Título: <input type="text" name=titulo>
-        Descricao: <input type="text" name=descricao>
-        <input type="file" name="foto">
-        <input type="submit" name="Submit" value="Enviar"> 
+        <select name="concurso">
+          <option value="0" selected>Natureza</option>
+          <option value="1">Pessoas</option>
+          <option value="2">Outros</option>
+        </select>
+      </p>
+      
+      Título: <input type="text" name=titulo>
+      Descricao: <input type="text" name=descricao>
+      <input type="file" name="foto">
+      <input type="submit" name="Submit" value="Enviar">
 
     </form>
+  </div>
 </body>
 
 </html>
