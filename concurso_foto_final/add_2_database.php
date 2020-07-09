5dc96a4fb23fbc8dcd967b4caf365e4726894de0
Teitei011 <stefantleal14@gmail.com>
2020-07-09 17:23:28 -0300

    dsahjgdsa

--- concurso_foto_final/add_2_database.php
+++ concurso_foto_final/add_2_database.php
@@ -28,36 +28,8 @@
     }
   }
 
-  // Check if file already exists
-  if (file_exists($target_file)) {
-      echo "Sorry, file already exists.";
-      $uploadOk = 0;
-    }
-    
-    // Check file size
-    if ($_FILES["foto"]["size"] > 500000) {
-      echo "Sorry, your file is too large.";
-      $uploadOk = 0;
-    }
-    
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-      $uploadOk = 0;
-    }
-    
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-      echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    } else {
-      if (move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["foto"]["name"]). " has been uploaded.";
-      } else {
-        echo "Sorry, there was an error uploading your file.";
-      }
-    }
+  move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file);
+
 
   $caminho = $_FILES["foto"]["tmp_name"];
 
