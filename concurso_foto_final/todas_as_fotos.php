9fb066584d03b11f063022d40484113c96bdd345
Teitei011 <stefantleal14@gmail.com>
2020-07-13 11:11:08 -0300

    dsads

--- concurso_foto_final/todas_as_fotos.php
+++ concurso_foto_final/todas_as_fotos.php
@@ -19,28 +19,11 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css"
         rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/todas_as_fotos.css" rel="stylesheet" />
 </head>
 
 <body id="page-top">
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-        <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top">Concurso de Fotografia</a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-
-        </div>
-    </nav>
-    <!-- Masthead-->
-    <header class="masthead">
-        <div class="container h-100">
-            <div class="row h-100 align-items-center justify-content-center text-center">
-                <div class="col-lg-10 align-self-end">
-                    <h1 class="text-uppercase text-white font-weight-bold">Inscreva-se</h1>
-                    <hr class="divider my-4" />
-                </div>
+   
                 <?php
   
   $db = new SQLite3('database.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
@@ -69,14 +52,14 @@
 
 ?>
                 
-                    <h2>Coloque aqui a ID da foto que você mais gostou</h2>
-                
-                <form class="form-signin" action="/~stefantcleal/concurso_foto_final/votar.php">
-                    <input type="text" name="Id" class="form-control" placeholder="ID" required="" autofocus="">
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">
-                        Submit
-                    </button>
-                </form>
+    <h2>Coloque aqui a ID da foto que você mais gostou</h2>
+
+<form class="form-signin" action="/~stefantcleal/concurso_foto_final/votar.php">
+    <input type="text" name="Id" class="form-control" placeholder="ID" required="" autofocus="">
+    <button class="btn btn-lg btn-primary btn-block" type="submit">
+        Submit
+    </button>
+</form>
 
             </div>
         </div>
