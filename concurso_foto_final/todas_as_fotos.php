7e7f46c6a5ba4cc98bb17b2b6f3ab44951acb57d
Teitei011 <stefantleal14@gmail.com>
2020-07-13 11:45:46 -0300

    dhsakjdhs

--- concurso_foto_final/todas_as_fotos.php
+++ concurso_foto_final/todas_as_fotos.php
@@ -45,7 +45,7 @@
         echo "<p><b> ID: $ID <K/b></p>";
         echo "<b> Quantidade de Votos: $votos</b>";
 
-        echo '<form action="/~stefantcleal/concurso_foto_final/votar.php?IDd='.$ID.'" method="get">';
+        echo '<form action="/~stefantcleal/concurso_foto_final/votar.php?ID='.$ID.'" method="get">';
             echo '<button type="submit" name='.$ID.'>Votar</button>';
         echo'</form>';
 
