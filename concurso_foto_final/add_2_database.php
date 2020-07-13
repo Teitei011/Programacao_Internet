7d5ac2a616cdc5a7e5ff9a37662969e2f57821c2
Teitei011 <stefantleal14@gmail.com>
2020-07-13 11:57:51 -0300

    add 2 database

--- concurso_foto_final/add_2_database.php
+++ concurso_foto_final/add_2_database.php
@@ -9,18 +9,23 @@
 
   $db = new SQLite3('database.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
+  echo "<br>Concurso: $concurso";
+  echo "<br>Titulo: $titulo";
+  echo "<br>Descricao: $descricao";
+  echo "<br>ID Artista: $ID_Artista";
+
 // Upload function
-  $target_dir = "assets/upload/";
-  $target_file = $target_dir . basename($_FILES["foto"]["name"]);
-  move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file);
+  // $target_dir = "assets/upload/";
+  // $target_file = $target_dir . basename($_FILES["foto"]["name"]);
+  // move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file);
 
-  $caminho = $_FILES["foto"]["tmp_name"];
+  // $caminho = $_FILES["foto"]["tmp_name"];
 
-  $db->exec("INSERT INTO  Photo(ID_Artist, titulo, descricao, votos, caminho, vai_participar) VALUES 
-  ('$ID_Artista', '$titulo', '$descricao', 0, ' $target_file', 1);");
+  // $db->exec("INSERT INTO  Photo(ID_Artist, titulo, descricao, votos, caminho, vai_participar) VALUES 
+  // ('$ID_Artista', '$titulo', '$descricao', 0, ' $target_file', 1);");
 	
-  $db->close();
+  // $db->close();
 
-  header("Location: http://fourier.ifsc.usp.br/~stefantcleal/concurso_foto_final/todas_as_fotos.php", true, 301);
+  // header("Location: http://fourier.ifsc.usp.br/~stefantcleal/concurso_foto_final/todas_as_fotos.php", true, 301);
   
 ?>
